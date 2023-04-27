6bcaa3fcb89ba6fca72b2b19dc185aa928a7f83b
myusername <myemail@gmail.com>
2023-04-26 19:11:32 -0700

    Tasks 12

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -7,8 +7,15 @@
  */
 int main(void)
 {
+    int r;
+
+    print_times_table(3);
     _putchar('\n');
+    print_times_table(5);
     _putchar('\n');
-    _putchar('\n');  
+    print_times_table(98);
+    _putchar('\n');
+     r = print_times_table(-1024);
+    print_times_table(12);  
     return (0);
 }
